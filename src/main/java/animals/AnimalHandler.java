6a17143a2e65932c6ff4283a65b171ccd5a5eed1
Henrique <diaslinoh@gmail.com>
2019-01-13 17:32:47 -0200

    Reading Animals from a JSON file.

--- src/main/java/animals/AnimalHandler.java
+++ src/main/java/animals/AnimalHandler.java
@@ -1,6 +1,12 @@
 package animals;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.StringJoiner;
 
 public class AnimalHandler {
@@ -10,16 +16,67 @@ public class AnimalHandler {
 
     // Add some animals objects to a list
     public void startAnimals() {
+        JSONParser parser = new JSONParser();
+        try {
 
-        listAnimals.add(new Dog("Killian", "Meat", "Hunting dog"));
-        listAnimals.add(new Dog("Rocky", "Fresh meat", "Working dog"));
-        listAnimals.add(new Dog("Peter", "Pedigree", "Sport dog"));
-        listAnimals.add(new Parrot("Parrote one", "Grain", false, 0.25f));
-        listAnimals.add(new Parrot("Parrote two", "Corn", true, 0.5f));
-        listAnimals.add(new Chicken("Chicken one", "Corn", true, 0.75f));
-        listAnimals.add(new Chicken("Chicken two", "Corn", false, 0.75f));
+            Object obj = parser.parse(new FileReader("animals.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+
+
+
+
+            // Adding dogs to the list
+            JSONArray dogs = (JSONArray) jsonObject.get("dog");
+            Iterator itDog = dogs.iterator();
+            while (itDog.hasNext()) {
+                JSONObject tmpDog = (JSONObject) itDog.next();
+                listAnimals.add(new Dog((String) tmpDog.get("name"),
+                        (String) tmpDog.get("favoriteFood"),
+                        (String) tmpDog.get("dogType")));
+            }
+
+
+
+            // Adding  parrots to the list
+
+            JSONArray parrot = (JSONArray) jsonObject.get("parrot");
+            Iterator itParrot = parrot.iterator();
+            while (itParrot.hasNext()) {
+                JSONObject tmpParrot = (JSONObject) itParrot.next();
+                String tmpName = (String) tmpParrot.get("name");
+                String tmpFavFood = (String) tmpParrot.get("favoriteFood");
+                Boolean tmpSpeak = (Boolean) tmpParrot.get("speak");
+                Float tmpWingLength = Float.parseFloat(((String) tmpParrot.get("wingLength")));
+
+                listAnimals.add(new Parrot(tmpName,tmpFavFood,tmpSpeak,tmpWingLength));
+            }
+
+
+            // Adding  chickens to the list
+            JSONArray chickens = (JSONArray) jsonObject.get("chicken");
+            Iterator itChicken = chickens.iterator();
+            while (itChicken.hasNext()) {
+                JSONObject tmpChicken = (JSONObject) itChicken.next();
+                listAnimals.add(new Chicken((String) tmpChicken.get("name"),
+                        (String) tmpChicken.get("favoriteFood"),
+                        (boolean) tmpChicken.get("broiler"),
+                        Float.parseFloat(((String) tmpChicken.get("wingLength")))));
+            }
+
+
+        } catch (Exception e) {
+
+        }
+//        listAnimals.add(new Dog("Killian", "Meat", "Hunting dog"));
+//        listAnimals.add(new Dog("Rocky", "Fresh meat", "Working dog"));
+//        listAnimals.add(new Dog("Peter", "Pedigree", "Sport dog"));
+//        listAnimals.add(new Parrot("Parrote one", "Grain", false, 0.25f));
+//        listAnimals.add(new Parrot("Parrote two", "Corn", true, 0.5f));
+//        listAnimals.add(new Chicken("Chicken one", "Corn", true, 0.75f));
+//        listAnimals.add(new Chicken("Chicken two", "Corn", false, 0.75f));
 
     }
+
     // Display all the animals and their friends.
     public void listAllAnimals(ArrayList<Animal> animals) {
         for (Animal a : animals) {
@@ -46,7 +103,7 @@ public class AnimalHandler {
     public void liveOneDay(ArrayList<Animal> animals) {
 
         // Remove one friend of each animal if there is any
-        animals.forEach((currAnimal)->{
+        animals.forEach((currAnimal) -> {
             try {
                 if (currAnimal.getFriends().size() > 0) {
                     if (currAnimal.loseFriendship(currAnimal.getFriends().get(0))) {
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,3 @@
-package com.zoo;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
